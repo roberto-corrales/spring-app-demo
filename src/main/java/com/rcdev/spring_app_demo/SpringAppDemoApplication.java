ad3c1e1cc01123295f7b547835d63007182e9e6a
Roberto Corrales <roberto.corrales@gmail.com>
2025-11-01 00:06:56 -0600

    first commit

---
+++ src/main/java/com/rcdev/spring_app_demo/SpringAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rcdev.spring_app_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcdev/spring_app_demo/SpringAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcdev.spring_app_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
